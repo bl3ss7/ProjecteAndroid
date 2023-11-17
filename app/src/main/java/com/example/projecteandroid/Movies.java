d688fbcd206e25e2d1f7b129dd61f2a788589526
Ingrid <ingrid.morar@educem.net>
2023-11-17 12:31:55 +0100

    boton 6

--- app/src/main/java/com/example/projecteandroid/Movies.java
+++ app/src/main/java/com/example/projecteandroid/Movies.java
@@ -1,5 +1,5 @@
 package com.example.projecteandroid;
-
+//INGRID
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
--- app/src/main/java/com/example/projecteandroid/Transport.java
+++ app/src/main/java/com/example/projecteandroid/Transport.java
@@ -1,5 +1,5 @@
 package com.example.projecteandroid;
-
+//BIEL
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
